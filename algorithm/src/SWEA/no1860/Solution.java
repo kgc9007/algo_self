eeb793e4c439b700895b0cfa4eff63b41945f073
kgc9007 <gic3dev@gamil.com>
2024-02-01 23:38:08 +0900

    240201 1140pm

--- algorithm/src/SWEA/no1860/Solution.java
+++ algorithm/src/SWEA/no1860/Solution.java
@@ -2,26 +2,81 @@
 
 package SWEA.no1860;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		// 테스트 케이스의 수 입력
 		int T = sc.nextInt();
+		int testCase = 0;
 		
 		while (T-- > 0) {
+			testCase++;
+			
 			// 전체 손님의 수 N
 			int N = sc.nextInt();
 			// M초의 시간마다 K개의 붕어빵을 만들 수 있음
 			int M = sc.nextInt();
 			int K = sc.nextInt();
+
+			// N명의 손님의 도착 시간을 arrivalTime 배열에 입력
+			int[] arrivalTime = new int[N];
+			for (int i = 0; i < N; i++) {
+				arrivalTime[i] = sc.nextInt();
+			}
+
+			// 정렬 후 마지막 손님의 도착 시간을 lastArrivalTime으로 저장
+			// 0초부터 시작이므로 1 추가한 값으로 설정
+			Arrays.sort(arrivalTime);
+			int lastArrivalTime = arrivalTime[N - 1] + 1;
+
+			// 마지막 손님 도착 시간까지
+			// 해당 시간의 손님 수를 저장할 배열 counting 생성, 값 입력
+			int[] counting = new int[lastArrivalTime];
+			for (int i = 0; i < N; i++) {
+				counting[arrivalTime[i]]++;
+			}
+
+			// 마지막 손님 도착 시간까지 붕어빵 재고 수를 표현할 배열 inventory 생성
+			int[] inventory = new int[lastArrivalTime];
+
+			// 0초 ~ 마지막 손님 도착까지
+			// M초가 지날때마다 붕어빵 K개 +
+			// counting[time]의 값이 0이 아니면
+			// -> 해당 시간에 손님이 방문할 예정이므로 counting[time] = 0이 될때까지 재고--
+
+			// 재고가 0보다 작아지는 경우 붕어빵을 바로 제공할 수 X -> isPossible = false
+			boolean isPossible = true;
+			for (int time = 0; time < lastArrivalTime; time++) {
+				if (time != 0) {
+					inventory[time] = inventory[time - 1];
+					if (time % M == 0) {
+						inventory[time] += K;
+					}
+				}
+
+				if (counting[time] != 0) {
+					inventory[time] -= counting[time];
+				}
+
+				if (inventory[time] < 0) {
+					isPossible = false;
+					break;
+				}
+			}
 			
-			
+			// 결과 출력
+			if (isPossible) {
+				System.out.printf("#%d Possible %n", testCase);
+			} else {
+				System.out.printf("#%d Impossible %n", testCase);
+			}
+
 		}
-		
-		
+
 	}
-	
+
 }
--- algorithm/src/SWEA/no7084/Solution.java
+++ algorithm/src/SWEA/no7084/Solution.java
@@ -9,8 +9,41 @@ public class Solution {
 		Scanner sc = new Scanner(System.in);
 		
 		int T = sc.nextInt();
-		for (int testCase = 1; testCase <- T; testCase++) {
+		for (int testCase = 1; testCase <= T; testCase++) {
 			int N = sc.nextInt();
+			
+			// A ~ Z까지 사용되었는지를 저장할 배열 isUsed 생성
+			boolean[] isUsed = new boolean[26];
+
+			
+			// 은기가 만든 문제의 제목을 입력받아서 
+			// 첫번째 글자에 해당하는 isUsed 배열의 값을 true로 변환
+			
+			// ASCII Code -  A : 65, B : 66, ...
+			
+			for (int i=0; i<N; i++) {
+				char firstText = sc.next().charAt(0);
+				isUsed[firstText-65] = true;
+			}
+			
+			// isUsed 배열을 0번부터 확인하면서
+			// 해당 알파벳이 사용되었으면 notSuedAlphabet++
+			// 해당 알파벳이 사용되지 않았으면 break
+			
+			// notUsedAlphabet이 0이면 A, 1이면 B, 2이면 C, ...
+			
+			int notUsedAlphabet = 0;
+			for (int i=0; i<26; i++) {
+				if (isUsed[i]) {
+					notUsedAlphabet++;
+				} else {
+					break;
+				}
+			}
+			
+			// 결과 출력
+			System.out.printf("#%d %d%n", testCase, notUsedAlphabet);
+			
 			
 		}
 		
