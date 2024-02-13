2207fadd98c66ebac45dd65680afc611d40d26a5
kgc9007 <gic3dev@gamil.com>
2024-02-13 22:51:03 +0900

    gitignore delete

--- algorithm/src/BOJ/no15829/Main.java
+++ algorithm/src/BOJ/no15829/Main.java
@@ -8,12 +8,15 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		// 문자열 길이 L, 문자열 입력
 		int L = sc.nextInt();
 		String str = sc.next();
 		
+		// 해시 값 계산
 		long result = 0;
 		for (int i=L-1; i>=0; i--) {
 			result = result * 31 + (str.charAt(i) - 96);
+			result %= 1234567891;
 		}
 
 		System.out.println(result);
