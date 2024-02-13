fdd6ba9b3b089ab3cd740f335df90c93442f49c2
kgc9007 <gic3dev@gamil.com>
2024-02-13 22:51:33 +0900

    240213 1050pm

---
+++ algorithm/src/BOJ/no18111/Main.java
@@ -0,0 +1,25 @@
+// BOJ 18111번 마인크래프트
+
+package BOJ.no18111;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int B = sc.nextInt();
+		
+		int[][] map = new int[N][M];
+		for (int r = 0; r < N; r++) {
+			for (int c = 0; c < M; c++) {
+				map[r][c] = sc.nextInt();
+			}
+		}
+		
+		int totalTime = 0;
+		
+	}
+}
