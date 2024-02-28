acbcc8aa895c9dd3cf27141013321e1a738914a1
kgc9007 <gic3dev@gamil.com>
2024-02-28 21:15:49 +0900

    240228 0915pm

--- mohaning/src/BOJ/no1991/Main.java
+++ mohaning/src/BOJ/no1991/Main.java
@@ -1,20 +1,21 @@
 // BOJ 1991번 트리 순회
+// 실패 - 노드 정보가 순서대로 주어지지 않는 경우 입력 실패
 
 package BOJ.no1991;
 
 import java.util.Scanner;
 
 public class Main {
-	
+
 	// 결과를 저장할 StringBuilder sb
 	static StringBuilder sb;
-	
+
 	// 트리를 표현할 배열 생성
 	static char[] tree = new char[27];
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		// N개의 줄에 걸쳐 트리의 정보 입력
 		int N = sc.nextInt();
 		while (N-- > 0) {
@@ -23,7 +24,7 @@ public class Main {
 			char V = sc.next().charAt(0);
 			char L = sc.next().charAt(0);
 			char R = sc.next().charAt(0);
-			
+
 			// 입력받은 정보를 배열에 저장
 			if (V == 'A') {
 				tree[1] = V;
@@ -52,23 +53,62 @@ public class Main {
 
 		}
 
+		// 전위 순회 실시
+		sb = new StringBuilder();
+		preorder(1);
+		// 결과 출력 - 전위 순회
+		System.out.println(sb);
+
+		// 중위 순회 실시
+		sb = new StringBuilder();
+		inorder(1);
+		// 결과 출력 - 전위 순회
+		System.out.println(sb);
+
+		// 후위 순회 실시
+		sb = new StringBuilder();
+		postorder(1);
+		// 결과 출력 - 전위 순회
+		System.out.println(sb);
+
 	}
-	
+
 	// 전위 순회
-	public static void preorder() {
-		sb = new StringBuilder();
+	// VLR
+	public static void preorder(int idx) {
+		if (idx >= tree.length || tree[idx] == '\0') {
+			return;
+		}
+
+		sb.append(tree[idx]);
+		preorder(idx * 2);
+		preorder(idx * 2 + 1);
+
 	}
-	
+
 	// 중위 순회
-	public static void inorder() {
-		sb = new StringBuilder();
-		
+	// LVR
+	public static void inorder(int idx) {
+		if (idx >= tree.length || tree[idx] == '\0') {
+			return;
+		}
+
+		inorder(idx * 2);
+		sb.append(tree[idx]);
+		inorder(idx * 2 + 1);
 	}
 
 	// 후위 순회
-	public static void postorder() {
-		sb = new StringBuilder();
-		
+	// LRV
+	public static void postorder(int idx) {
+		if (idx >= tree.length || tree[idx] == '\0') {
+			return;
+		}
+
+		postorder(idx * 2);
+		postorder(idx * 2 + 1);
+		sb.append(tree[idx]);
+
 	}
-	
+
 }
