7cf125920d0b3d2ca663aca84c66d0da5b258007
kgc9007 <gic3dev@gamil.com>
2024-03-05 23:58:41 +0900

    ADD 240305

---
+++ algorithm/src/BOJ/no9663/Main.java
@@ -0,0 +1,29 @@
+// BOJ 9663번 N-Queen
+
+package BOJ.no9663;
+
+import java.util.Scanner;
+
+public class Main {
+	// 탐색을 위한 델타배열
+	static int[] dr = { -1, -1, -1 };
+	static int[] dc = { -1, 0, 1 };
+
+	static int N;
+	static boolean[][] board;
+
+	static int count;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+		board = new boolean[N][N];
+		
+		
+	}
+
+	public static void solve() {
+		
+	}
+}
