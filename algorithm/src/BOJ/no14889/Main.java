aa8bcd353e13f50942b1da96b42177b9d7f56225
kgc9007 <gic3dev@gamil.com>
2024-03-07 00:53:09 +0900

    ADD 240306

---
+++ algorithm/src/BOJ/no14889/Main.java
@@ -0,0 +1,11 @@
+// BOJ 14889번 스타트와 링크
+
+package BOJ.no14889;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
--- algorithm/src/BOJ/no9663/Main.java
+++ algorithm/src/BOJ/no9663/Main.java
@@ -19,11 +19,53 @@ public class Main {
 
 		N = sc.nextInt();
 		board = new boolean[N][N];
-		
-		
+
+		// count 초기화 후 백트래킹 실시
+		count = 0;
+		solve(0);
+
+		// 결과 출력
+		System.out.println(count);
+
 	}
 
-	public static void solve() {
-		
+	// 백트래킹
+	public static void solve(int r) {
+		// 마지막 행까지 퀸을 추가한 후 서로 공격하지 않으면 
+		// -> count를 증가시킨 후 return
+		if (r == N) {
+			count++;
+			return;
+		}
+
+		// 마지막 행 이전의 경우
+		// 0번 열부터 순서대로 퀸을 놓아보고 이미 놓아진 퀸과 서로 공격하지 않는지 확인
+		// 서로 공격하지 않는다면 -> possible = true, 다음 행으로 이동
+		// 서로 공격한다면 -> possible = false, 다음 열로 이동
+		for (int c = 0; c < N; c++) {
+			board[r][c] = true;
+
+			boolean possible = true;
+			for (int d = 0; d < 3; d++) {
+				int nr = r + dr[d];
+				int nc = c + dc[d];
+
+				while (nr >= 0 && nr < N && nc >= 0 && nc < N) {
+					if (board[nr][nc]) {
+						possible = false;
+						break;
+					}
+					nr += dr[d];
+					nc += dc[d];
+				}
+				if (!possible) {
+					break;
+				}
+			}
+			if (possible) {
+				solve(r + 1);
+			}
+			board[r][c] = false;
+		}
 	}
 }
