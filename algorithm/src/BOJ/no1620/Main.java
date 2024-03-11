49069f2417534e4bbabfa674516f77b7c962d9a6
kgc9007 <gic3dev@gmail.com>
2024-03-11 16:27:36 +0900

    240311

---
+++ algorithm/src/BOJ/no1620/Main.java
@@ -0,0 +1,15 @@
+// BOJ 1620번 나는야 포켓몬 마스터 이다솜
+
+package BOJ.no1620;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+	}
+}
