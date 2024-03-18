55ce2cffb424469b695bb53e1eb1f019c4baf03b
kgc9007 <gic3dev@gmail.com>
2024-03-19 01:04:36 +0900

    ADD 240318

---
+++ mohaning/src/BOJ/no15486/Main.java
@@ -0,0 +1,25 @@
+// BOJ 15486번 퇴사 2
+
+package BOJ.no15486;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int[][] info = new int[N][2];
+
+		for (int i = 0; i < N; i++) {
+			info[i][0] = sc.nextInt();
+			info[i][1] = sc.nextInt();
+		}
+
+		// dp 배열 생성
+		int[] dp = new int[N];
+		
+
+
+	}
+}
