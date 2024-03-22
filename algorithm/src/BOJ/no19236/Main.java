951251e187feb55e19a0398a2f85c9a9bf6dcec1
kgc9007 <gic3dev@gmail.com>
2024-03-23 01:59:44 +0900

    ADD 240322

---
+++ algorithm/src/BOJ/no19236/Main.java
@@ -0,0 +1,18 @@
+// BOJ 19236번 청소년 상어
+// https://www.acmicpc.net/problem/19236
+
+package BOJ.no19236;
+
+import java.util.Scanner;
+
+public class Main {
+	// 델타 배열
+	static int[] dr = { -1, -1, 0, 1, 1, 1, 0, -1 };
+	static int[] dc = { 0, -1, -1, -1, 0, 1, 1, 1 };
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+}
