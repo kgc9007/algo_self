7f7d601c26b312701e8b77926cd36c28170f9a2b
kgc9007 <gic3dev@gmail.com>
2024-03-28 00:15:35 +0900

    -ing 240327

---
+++ algorithm/src/BOJ/no17825/Main.java
@@ -0,0 +1,14 @@
+// BOJ 17825번 주사위 윷놀이
+// https://www.acmicpc.net/problem/17825
+
+package BOJ.no17825;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+}
