e2f60af5941ed5543725bfcfe57770991e17058c
kgc9007 <gic3dev@gmail.com>
2024-03-28 00:15:11 +0900

    ADD 240327

--- algorithm/src/BOJ/no20055/Main.java
+++ algorithm/src/BOJ/no20055/Main.java
@@ -3,35 +3,96 @@
 
 package BOJ.no20055;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	static int N;
 	static int K;
-	
+
 	static int[][] belt;
-	
-	static List<Integer> robot = new ArrayList<>();
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		N = sc.nextInt();
 		K = sc.nextInt();
-		
+
+		// 컨베이어 벨트의 정보 (내구도) 입력
 		belt = new int[2 * N + 1][2];
 		for (int i = 1; i <= 2 * N; i++) {
 			belt[i][0] = sc.nextInt();
 		}
+
+		// 시간 0 부터 시작
+		// 내구도가 0인 벨트가 K개 이상이면 종료 (flag = false)
+		int time = 0;
+		boolean flag = true;
+		while (flag) {
+			time++;
+			rotate();
+			move();
+			addRobot();
+			if (getCount() >= K) {
+				flag = false;
+			}
+		}
+
+		// 결과 출력
+		System.out.println(time);
 	}
-	
+
 	// 벨트 회전 메소드
 	public static void rotate() {
-		for (int i = belt.length - 1; i > 0; i--) {
-			belt[i][0] = belt[i-1][0];
-			belt[i][1] = belt[i-1][1];
+		// 한칸씩 시계 방향으로 벨트 회전
+		belt[0][0] = belt[2 * N][0];
+		for (int i = 2 * N; i > 0; i--) {
+			belt[i][0] = belt[i - 1][0];
+			belt[i][1] = belt[i - 1][1];
 		}
+
+		// 회전 이후 N번 칸의 위치에 로봇이 있다면 로봇을 내림
+		if (belt[N][1] == 1) {
+			belt[N][1] = 0;
+		}
+
+	}
+
+	// 로봇 이동 메소드
+	public static void move() {
+		// N - 1번째 칸부터 1번 칸까지 순서대로 확인
+		// 해당 칸에 로봇이 있다면
+		// 다음칸의 내구도가 0이 아니고 다음칸이 빈 칸인 경우 이동
+		for (int i = N - 1; i > 1; i--) {
+			if (belt[i][1] == 1 && belt[i + 1][0] > 0 && belt[i + 1][1] != 1) {
+				belt[i][1] = 0;
+				belt[i + 1][1] = 1;
+
+				belt[i + 1][0]--;
+			}
+		}
+
+		// 이동 이후 N번 칸의 위치에 로봇이 있다면 로봇을 내림
+		if (belt[N][1] == 1) {
+			belt[N][1] = 0;
+		}
+	}
+
+	// 로봇을 올리는 메소드
+	public static void addRobot() {
+		if (belt[1][0] != 0) {
+			belt[1][1] = 1;
+			belt[1][0]--;
+		}
+	}
+
+	// 내구도가 0인 칸의 개수 파악
+	public static int getCount() {
+		int count = 0;
+		for (int i = 1; i <= 2 * N; i++) {
+			if (belt[i][0] == 0) {
+				count++;
+			}
+		}
+		return count;
 	}
 }
