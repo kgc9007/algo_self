c29020ed1b04dc09cffc4659dc284374698d76a9
kgc9007 <gic3dev@gmail.com>
2024-04-05 00:13:33 +0900

    ADD 240404

---
+++ mohaning/src/BOJ/no17140/Main.java
@@ -0,0 +1,14 @@
+// BOJ 17140번 이차원 배열과 연산
+// https://www.acmicpc.net/problem/17140
+
+package BOJ.no17140;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+}
