97aabbe11c0f8d98bc2aafa08d54ee930330160d
kgc9007 <gic3dev@gmail.com>
2024-04-23 23:33:09 +0900

    ADD 240423

--- mohaning/src/BOJ/no11559/Main.java
+++ mohaning/src/BOJ/no11559/Main.java
@@ -14,15 +14,18 @@ public class Main {
 	static int[] dr = { -1, 1, 0, 0 };
 	static int[] dc = { 0, 0, -1, 1 };
 
+	// 필드의 크기 : 12(행) * 6(열)
 	static int N = 12;
 	static int M = 6;
 	static char[][] map = new char[N][M];
 
+	// bfs를 위한 방문배열
 	static boolean[][] visited;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
+		// 필드 정보 입력
 		for (int r = 0; r < N; r++) {
 			String line = sc.next();
 			for (int c = 0; c < M; c++) {
@@ -30,19 +33,32 @@ public class Main {
 			}
 		}
 
-		visited = new boolean[N][M];
-		boolean isEnd = true;
-		for (int r = 0; r < N; r++) {
-			for (int c = 0; c < M; c++) {
-				if (map[r][c] != '.') {
-					boolean exploded = bfs(r, c, map[r][c]);
+		// 더 이상 터질 수 있는 뿌요가 없을 때까지 반복
+		boolean isEnd = false;
+		int time = -1;
+		while (!isEnd) {
+			time++;
+			// 방문 배열 초기화 후 전체 필드를 순회하며 bfs 실시
+			visited = new boolean[N][M];
+			isEnd = true;
+			for (int r = 0; r < N; r++) {
+				for (int c = 0; c < M; c++) {
+					if (map[r][c] != '.' && !visited[r][c]) {
+						boolean exploded = bfs(r, c, map[r][c]);
 
-					if (exploded) {
-						isEnd = false;
+						// bfs 결과 뿌요가 4개 이상 모여 터지게 되었다면 isEnd = false로 변경
+						if (exploded) {
+							isEnd = false;
+						}
 					}
 				}
 			}
+			// 중력의 영향을 받아 뿌요들이 아래로 떨어지는 과정
+			move();
 		}
+
+		// 결과 출력
+		System.out.println(time);
 
 	}
 
@@ -92,7 +108,26 @@ public class Main {
 
 	// 뿌요들이 터진 후 남은 뿌요들이 이동하는 메소드
 	public static void move() {
-		
+		// 각 열별로 가장 마지막 행부터 순서대로 확인
+		// 뿌요가 남아있다면 리스트에 추가
+		for (int c = 0; c < M; c++) {
+			List<Character> list = new ArrayList<>();
+			for (int r = N - 1; r >= 0; r--) {
+				if (map[r][c] != '.') {
+					list.add(map[r][c]);
+				}
+			}
+
+			// 리스트에 추가된 순으로 가장 아래 행부터 map에 입력
+			int idx = N - 1;
+			for (int i = 0; i < list.size(); i++) {
+				map[idx--][c] = list.get(i);
+			}
+			// 남은 칸은 모두 '.'으로 입력
+			for (int i = list.size(); i < N; i++) {
+				map[idx--][c] = '.';
+			}
+		}
 	}
 
 	// 경계를 벗어났는지 확인하는 메소드
